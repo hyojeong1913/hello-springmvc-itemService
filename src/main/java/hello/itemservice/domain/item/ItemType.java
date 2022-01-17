acee8e24ff176a7ef757230ae91c8d02cdaeebb5
LEE HYO JEONG <hyojeong1913@naver.com>
2022-01-18 00:39:53 +0900

    요구사항 추가

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,16 @@
+package hello.itemservice.domain.item;
+
+/**
+ * 상품 종류 ENUM
+ */
+public enum ItemType {
+
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    // 상품 설명
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
