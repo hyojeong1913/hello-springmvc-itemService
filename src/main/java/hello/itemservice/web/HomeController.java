7c8b78aae24156ccfa17d67a4bf919d493299231
LEE HYO JEONG <hyojeong1913@naver.com>
2022-02-22 00:22:26 +0900

    로그인 처리하기 - 서블릿 HTTP 세션2

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -70,7 +71,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(
             HttpServletRequest request,
             Model model
@@ -86,6 +87,33 @@ public class HomeController {
 
         // 로그인 시점에 세션에 보관한 회원 객체 조회
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 로그인 홈으로 이동
+        if (loginMember == null) {
+
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
+    }
+
+    /**
+     * 스프링은 세션을 더 편리하게 사용할 수 있도록 @SessionAttribute 을 지원
+     *
+     * @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+     * : 이미 로그인된 사용자를 찾을 때 사용, 이 기능은 세션을 생성하지 않는다.
+     *
+     * @param loginMember
+     * @param model
+     * @return
+     */
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model
+    ) {
 
         // 세션에 회원 데이터가 없으면 로그인 홈으로 이동
         if (loginMember == null) {
