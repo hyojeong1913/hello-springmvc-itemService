d7537be94d1246eda4fcb44a4a70b3a580432e33
LEE HYO JEONG <hyojeong1913@naver.com>
2022-03-13 00:45:11 +0900

    스프링이 제공하는 ExceptionResolver2

--- src/main/java/hello/itemservice/web/exception/api/ApiExceptionController.java
+++ src/main/java/hello/itemservice/web/exception/api/ApiExceptionController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -68,6 +69,25 @@ public class ApiExceptionController {
     public String responseStatusEx2() {
 
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    /**
+     * TypeMismatchException 예외
+     *
+     * DefaultHandlerExceptionResolver 는 스프링 내부 오류를 어떻게 처리할지 수 많은 내용이 정의되어 있어
+     * 스프링 내부에서 발생하는 스프링 예외를 해결
+     *
+     * 대표적으로 파라미터 바인딩 시점에 타입이 맞지 않으면 내부에서 TypeMismatchException 이 발생
+     * => 그냥 두면 서블릿 컨테이너까지 오류가 올라가고, 결과적으로 500 오류 발생
+     * => 파라미터 바인딩은 대부분 클라이언트가 HTTP 요청 정보를 잘못 호출해서 발생하는 문제로 HTTP 에서는 HTTP 상태 코드 400을 사용
+     *
+     * @param data
+     * @return
+     */
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+
+        return "OK";
     }
 
     @Data
